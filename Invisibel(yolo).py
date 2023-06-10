d41a3b4da7dd1f2e9d9f5b5fb902428845dbbf0a
조인혁 <134765214+inyeok07@users.noreply.github.com>
2023-06-10 14:24:57 +0900

    x로 처리된 나머지 삭제

--- Invisibel(yolo).py
+++ Invisibel(yolo).py
@@ -13,17 +13,21 @@ COCO_LABELS.insert(0,col)
 webcam = cv.VideoCapture(0)
 while webcam.isOpened():
     ret, img = webcam.read()
+
     if not ret:
         print("Can't read camera")
         break
+
     cv.imshow("PC_camera", img)
     status, frame = webcam.read()
     frame = cv.cvtColor(frame,cv.COLOR_BGR2RGB) #화면을 인식할 프레임 생성
     preds = model(model.preprocess_input(frame)) #생성한 프레임으로 모델생성
     bboxs, labels, confidences = model.decode_predictions(preds)[0]
-    if status:
-        print([COCO_LABELS[i] for i in labels.numpy().tolist()])
 
+    if status:
+        k_label =str([COCO_LABELS[i] for i in labels.numpy().tolist()])
+        re_label = k_label.replace('x','')
+        print(re_label)
     if cv.waitKey(1) & 0xFF == ord('q'): #q를 누를시 작동 종료
         print('off')
         break
